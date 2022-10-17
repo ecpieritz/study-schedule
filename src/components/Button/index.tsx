f8d0fefabe54d7c8218e57d7bdafbed6afe010d7
Emilyn Pieritz <66131506+ecpieritz@users.noreply.github.com>
2022-10-17 15:21:13 -0300

    Added Button

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+class Button extends React.Component {
+  render(): React.ReactNode {
+    return (
+      <button>olá</button>
+    )
+  }
+}
+
+export default Button;
